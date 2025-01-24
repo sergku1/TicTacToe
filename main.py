b6078429995f78eeeb9fdd07157558f989e4d1e6
Sergey Kushnerov <arepo8@gmail.com>
2025-01-24 10:43:40 +0200

    READM

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-print('Start Game TikTakToe multicell variant')
-lintowin = input('input cell in line to win ')  # how cell to win
-points = {}  # dictionary i, j they have vector for each point
-lineX = {}  # dictionary kur kaupem sujungtas taskus su X
-line0 = {}  # dictionary kur kaupem sujungtas taskus su 0
-#  Понятие vector направление соседства одноименной ячейки. подобно направлениям стрелок на механических часах
-#  1 с лева, 2 - с лева - вверх, 3 - вверх, 4 - вверх - вправо
-# Taip jeigu paspaustas X or 0 pasirinktam langeleje
-
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+import sys
+import os
+import hashlib
+import shutil
+from PyQt5.QtWidgets import QApplication, QWidge
+from PyQt5 import QtWidgets, QtCore
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPixmap
+import smtplib
+# Импорт класса для обработки неизвестных MIME-типов, базирующихся на расширении файла
+import keyboard
+from pdf2image import convert_from_path
+import gui_tictactoe
 
 def points_scan(xdat, ival, jval):
     points[str(ival) + '-' + str(jval)] = xdat
@@ -90,26 +96,59 @@ def numb(chi):
             k, m = -1, -1
     return k, m
 
+class MyWindow(QtWidgets.QMainWindow, gui_tictactoe.Ui_MainWindow):
+    def __init__(self, parent=None):
+        super().__init__()
+        self.centralSpinPoint = None
+        self.setupUi(self)  #
+        QtCore.QTimer.singleShot(0, self.pushButton_6.setFocus)
+        app = QApplication(sys.argv)
+        window = QWidget()
+        window.show()
+        self.pixmap = QPixmap(img_file_full)  #
+        self.label_95.setPixmap(self.pixmap)
 
-sq = int(input('input X size of playng deck X x X = '))
-ttt = 1
-while True:
-    if ttt % 2 != 0:
-        print('You play X')
-        #  global xvalue
-        xvalue = 'X'
-    else:
-        print('You play 0')
-        #  global xvalue
-        xvalue = '0'
-    xdate = ['']
-    play = input('please input Your step in format i-j = ')
-    ivalue = int(play.split('-')[0])  # Counter i, axis X
-    jvalue = int(play.split('-')[1])  # Counter j, axis Y
-    xdate[0] = xvalue
-    # points[str(ivalue) + '-' + str(jvalue)] = xdate  # insert X or 0 in point with coordinates i,j
-    points_scan(xdate, ivalue, jvalue)
-    print(points)
-    print(f'lineX = {lineX}')
-    print(f'line0 = {line0}')
-    ttt += 1
+    print('Start Game TikTakToe multicell variant')
+    lintowin = input('input cell in line to win ')  # how cell to win
+    points = {}  # dictionary i, j they have vector for each point
+    lineX = {}  # dictionary kur kaupem sujungtas taskus su X
+    line0 = {}  # dictionary kur kaupem sujungtas taskus su 0
+    lineSX = {}  # dictionary kur kaupem sujungtas linijos su X
+    lineS0 = {}  # dictionary kur kaupem sujungtas linijos su 0
+    #  Понятие vector направление соседства одноименной ячейки. подобно направлениям стрелок на механических часах
+    #  1 с лева, 2 - с лева - вверх, 3 - вверх, 4 - вверх - вправо
+    # Taip jeigu paspaustas X or 0 pasirinktam langeleje
+
+
+
+
+    sq = int(input('input X size of playng deck X x X = '))
+    ttt = 1
+    while True:
+        if ttt % 2 != 0:
+            print('You play X')
+            #  global xvalue
+            xvalue = 'X'
+        else:
+            print('You play 0')
+            #  global xvalue
+            xvalue = '0'
+        xdate = ['']
+        play = input('please input Your step in format i-j = ')
+        ivalue = int(play.split('-')[0])  # Counter i, axis X
+        jvalue = int(play.split('-')[1])  # Counter j, axis Y
+        xdate[0] = xvalue
+        # points[str(ivalue) + '-' + str(jvalue)] = xdate  # insert X or 0 in point with coordinates i,j
+        points_scan(xdate, ivalue, jvalue)
+        print(points)
+        print(f'lineX = {lineX}')
+        print(f'line0 = {line0}')
+        ttt += 1
+
+
+if __name__ == "__main__":
+    #   import sys
+    app = QtWidgets.QApplication(sys.argv)
+    window = MyWindow()
+    window.show()
+    sys.exit(app.exec_())
